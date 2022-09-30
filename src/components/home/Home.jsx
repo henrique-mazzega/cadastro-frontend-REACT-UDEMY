5ad163905712ec894796cd4a418590de0f1925d4
Henrique-Mazzega <henrique_lm08@hotmail.com>
2022-09-30 19:29:04 -0300

    first commit

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início" subtitle="Cadastro de Usuário">
+        <div className='display-4'>Bem Vindo!</div>
+    </Main>
---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+<footer className="footer">
+    <span>
+        Desenvolvido por <strong>Bruna</strong>. <i className="fa fa-heart text-danger"></i>
+    </span>
+</footer>
